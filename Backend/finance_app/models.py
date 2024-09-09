e69d63e6cad30072a27b95cf9b670c4e4aff8055
Zafir <shamsizafir@gmail.com>
2024-09-09 18:44:02 -0400

    od backend

--- Backend/finance_app/models.py
+++ Backend/finance_app/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from djongo import models
 
-# Create your models here.
+class Transaction(models.Model):
+    TRANSACTION_TYPES = [
+        ('income', 'Income'),
+        ('expense', 'Expense')
+    ]
+
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=100)
+    transaction_type = models.CharField(max_length=7, choices=TRANSACTION_TYPES)
+    date = models.DateField()
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.transaction_type.capitalize()} - {self.category}: {self.amount} on {self.date}"
